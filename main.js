88680d73aa45f6dc276795c2fe42fc02e0612652
George Uzoh <ugochukwuuzoh@gmail.com>
2023-10-02 00:48:43 +0300

    add game functions

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const playBtn = document.querySelector('#play-btn');
 const stopBtn = document.querySelector('#stop-btn');
 const keys = document.querySelectorAll('.key');
 const score = document.querySelector('.result');
+const modalDisplay = document.querySelector('#modal-msg');
+const overlay = document.querySelector('.modal-overlay');
 
 // Global variables
 let gameScore = 0;
 let timer;
 let pace = 1000;
-let currentKey = 0;
+let active = 0;
 let rounds = 0;
 
 // Random function
@@ -16,57 +18,55 @@ const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)
 
 const clickCircle = (i) => {
 
-    if (i !== currentKey) {
+    if (i !== active) {
         return endGame();
     }
 
-    console.log(`Circle at index ${i} was clicked`);
     rounds--;
     gameScore += 1;
     score.textContent = gameScore;
 }
 
-// const enableEvents = () => {
-//     keys.forEach(key => {
-//         key.st
-//     })
-// }
+keys.forEach((key, i) => {
+    key.addEventListener('click', () => clickCircle(i));
+});
 
-// Start game function
+const enableEvents = () => {
+    keys.forEach(key => {
+        key.style.pointerEvents = 'auto';
+    });
+}
+
 const startGame = () => {
     if (rounds >= 3) return endGame();
 
-    const activeKey = pickNewKey(currentKey);
+    enableEvents();
+    const newActive = pickNew(active);
 
-    keys[activeKey].classList.toggle('active');
-    keys[currentKey].classList.remove('active');
+    keys[newActive].classList.toggle('active');
+    keys[active].classList.remove('active');
 
-    // Sets the value of activeKey to currentKey
-    currentKey = activeKey;
+    active = newActive;
 
     timer = setTimeout(startGame, pace);
+
     pace -= 10;
     rounds++;
-    
-    function pickNewKey(currentKey) {
-        const activeKey = getRandomNumber(0, 3);
-        if (activeKey !== currentKey) {
-            return activeKey;
-        }
-        return pickNewKey(currentKey);
+
+    function pickNew(active) {
+        const newActive = getRandomNumber(0, 3);
+        if (newActive !== active) return newActive;
+
+        return pickNew(active);
     }
-    console.log(currentKey);
 }
 
-// End game function
 const endGame = () => {
-    console.log('Game ended!');
     clearTimeout(timer);
 
     resetGame();
 }
 
-// Reset game function
 const resetGame = () => {
     window.location.reload();
 }
@@ -74,6 +74,3 @@ const resetGame = () => {
 // Add event listeners to variables
 playBtn.addEventListener('click', startGame);
 stopBtn.addEventListener('click', endGame);
-keys.forEach((key, i) => {
-    key.addEventListener('click', () => clickCircle(i));
-});
