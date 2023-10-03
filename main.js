bac48892da8704bed924d40e6311f04dd34779d3
George Uzoh <ugochukwuuzoh@gmail.com>
2023-10-03 10:02:15 +0300

    add new piano layout

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ const score = document.querySelector('.result');
 const modalDisplay = document.querySelector('#modal-msg');
 const overlay = document.querySelector('.modal-overlay');
 const modalCloseBtn = document.querySelector('.modal-btn');
+const doh = document.querySelector('#doh');
+const re = document.querySelector('#re');
+const mi = document.querySelector('#mi');
+const fa = document.querySelector('#fa');
+const sol = document.querySelector('#sol');
+const la = document.querySelector('#la');
+const ti = document.querySelector('#ti');
+const highDoh = document.querySelector('#high-doh');
+const theme = document.querySelector('#theme');
 
 // Global variables
 let gameScore = 0;
@@ -15,35 +24,11 @@ let active = 0;
 let rounds = 0;
 let gameThemeSong;
 let pianoSound;
-const doh = new sound('do-80236.mp3');
-const re = new sound('re-78500.mp3');
-const mi = new sound('mi-80239.mp3');
-const fa = new sound('fa-78409.mp3');
-const sol = new sound('sol-101774.mp3');
-const la = new sound('la-80237.mp3');
-const ti = new sound('si-80238.mp3');
-
-// Game sound function - adapted from w3schools
-function sound(src) {
-    this.sound = document.createElement('audio');
-    this.sound.src = src;
-    this.sound.setAttribute('preload', 'auto');
-    this.sound.setAttribute('controls', 'none');
-    this.sound.style.display = 'none';
-    document.body.appendChild(this.sound);
-    this.play = function() {
-        this.sound.play();
-    }
-    this.stop = function() {
-        this.sound.pause();
-    }
-}
 
 // Random function
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-
-clickPlay = () => {
+const clickPlay = () => {
     if (clickSound.paused) clickSound.play();
     else clickSound.currentTime = 0;
 }
@@ -53,16 +38,17 @@ const clickCircle = (i) => {
     if (i !== active) {
         return endGame();
     } else if (i === active) {
-        if ((i === 0) || (i === 7)) pianoSound = doh;
+        if (i === 0) pianoSound = doh;
         else if (i === 1) pianoSound = re;
         else if (i === 2) pianoSound = mi;
         else if (i === 3) pianoSound = fa;
         else if (i === 4) pianoSound = sol;
         else if (i === 5) pianoSound = la;
         else if (i === 6) pianoSound = ti;
+        else if (i === 7) pianoSound = highDoh;
         
         pianoSound.play();
-        clickPlay();
+        // clickPlay();
     }
 
     rounds--;
@@ -82,6 +68,7 @@ const enableEvents = () => {
 
 stopBtn.classList.add('hide');
 
+// Game in progress
 const startGame = () => {
 
     playBtn.classList.add('hide');
@@ -103,7 +90,7 @@ const startGame = () => {
     rounds++;
 
     function pickNew(active) {
-        const newActive = getRandomNumber(0, 7);
+        const newActive = getRandomNumber(0, 11);
         if (newActive !== active) return newActive;
 
         return pickNew(active);
@@ -112,22 +99,19 @@ const startGame = () => {
 
 const showModal = () => overlay.classList.toggle('visible');
 
+// Game ends
 const endGame = () => {
     clearTimeout(timer);
 
     showModal()
 
-    gameThemeSong = new sound('dark-evil-piano-32205.mp3');
+    gameThemeSong = theme;
     gameThemeSong.play();
-    
-    // resetGame();
 }
 
 const resetGame = () => {
     window.location.reload();
 }
-
-
 
 // Add event listeners to variables
 playBtn.addEventListener('click', startGame);
